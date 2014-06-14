d2fe6d7889ecf2ecb187099fff7ab6974a6c719a
Luis Fernández <lfergon@gmail.com>
2014-06-14 23:08:57 +0200

    Fixed bug

--- src/server/server.js
+++ src/server/server.js
@@ -3,20 +3,20 @@ Meteor.startup(function () {
   if(Incidents.find().fetch().length===0){
     Log.error("New information avalaible on database");
     var incidents = [
-      {status: "Closed", delegation: "scheduled", commentHistory: ["Open ticket", "Data loaded"], priority: "high"},
-      {status: "Open", delegation: "scheduled", commentHistory: ["Open ticket", "Data loaded"], priority: "low"},
+      {createdAt: moment().valueOf(), status: "Closed", delegation: "scheduled", commentHistory: ["Open ticket", "Data loaded"], priority: "high"},
+      {createdAt: moment().valueOf(), status: "Open", delegation: "scheduled", commentHistory: ["Open ticket", "Data loaded"], priority: "low"}
     ];
     var buildings = [
-      {createdAt: moment().valueOf(), contact: "Luis Fernandez", email: "", phone: "", lat: , lng: , neighboorhood: "", temperature: [24, 21, 45, 18, 20, 23, 26], high: 20, area: 200, , description: "Corrupted read", incident: ""}, 
-      {createdAt: moment().valueOf(), building: "", contact: "David Grajal", lat: , lng: , temperature:[24, 21, 45, 44, 45, 23, 26], high: 20, priority: "low", description: "No data", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Julie Strd", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "medium", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "low", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
-      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: , lng: , temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), contact: "Luis Fernandez", email: "", phone: "", lat: 0, lng: 0, neighboorhood: "", temperature: [24, 21, 45, 18, 20, 23, 26], high: 20, area: 200, description: "Corrupted read", incident: ""}, 
+      {createdAt: moment().valueOf(), building: "", contact: "David Grajal", lat: 0, lng: 0, temperature:[24, 21, 45, 44, 45, 23, 26], high: 20, priority: "low", description: "No data", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Julie Strd", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "medium", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "low", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
+      {createdAt: moment().valueOf(), building: "", contact: "Luis Fernandez", lat: 0, lng: 0, temperature:[24, 21, 45, 18, 20, 23, 26], high: 20, priority: "high", description: "Corrupted read", incident: ""},
     ];
     
   }
