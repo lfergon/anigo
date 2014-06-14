ab256c6ac3d9d34adc775d883a747a7a7fe786f4
Luis Fernández <lfergon@gmail.com>
2014-06-14 21:01:22 +0200

    Map view created

--- src/client/incidents.js
+++ src/client/incidents.js
@@ -5,3 +5,20 @@ Template.incidents.events({
   }
 });
 
+Template.incidents.rendered = function () {
+  var mapContainer = document.getElementById("map");
+  console.log($("#map"));
+  
+  // Create a map inside the map container DOM node
+  var map = new nokia.maps.map.Display(mapContainer, {
+    // initial center and zoom level of the map
+    center: [52.51, 13.4],
+    zoomLevel: 10
+  });
+};
+
+Template.incidents.changeMap = function () {
+  Meteor.defer(function() {
+    $($("#map").children()[0]).css("position", "absolute");
+  });
+};
