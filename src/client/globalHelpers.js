ed40da218fb32d100dc7af83b86c57bd5c5126c1
Allard van Helbergen <allard@brandwatch.com>
2014-06-15 08:53:28 +0200

    Add priority icon to list

--- src/client/globalHelpers.js
+++ src/client/globalHelpers.js
@@ -13,3 +13,15 @@ UI.registerHelper('formatTime', function (time) {
 UI.registerHelper('loadInfoBuilding', function (id) {
   return Buildings.findOne({_id: id}).neighboorhood;
 });
+
+UI.registerHelper('priorityColour', function(priority) {
+  if (priority === 'low') {
+    return 'text-muted';
+  } else if (priority === 'medium') {
+    return 'text-info';
+  } else if (priority === 'high') {
+    return 'text-warning';
+  } else if (priority === 'critical') {
+    return 'text-danger';
+  }
+})
